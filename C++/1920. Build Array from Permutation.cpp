78e7378d7013675bd5dcf62d2831bd3e60c34dec
janyberg <jacob-nyberg@uiowa.edu>
2025-10-22 15:04:26 -0500

    1920. Build Array from Permutation
    beat 100

---
+++ C++/1920. Build Array from Permutation.cpp
@@ -0,0 +1,13 @@
+// Given a zero-based permutation nums (0-indexed), build an array ans of the same length where 
+//   ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+// A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+vector<int> buildArray(vector<int>& nums) {
+  std::vector<int> ret;
+  for (int i = 0; i < nums.size(); i++){
+    ret.push_back(nums[nums[i]]);
+  }
+  return ret;
+}
+// beats 100%
